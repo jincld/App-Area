b111495297618c825801904eff8b517728c99c84
Jonathan <Jonathan>
2024-03-19 14:54:00 -0600

    Subiendo

---
+++ app/src/main/java/jonathan/orellana/apparea/Calcular.kt
@@ -0,0 +1,9 @@
+package jonathan.orellana.apparea
+
+class Calcular {
+    fun calculo(num1: Int, num2: Int):Int{
+        val calculo1 = num1 * num2
+        val calculo2 = calculo1/2
+        return calculo2
+    }
+}
---
+++ app/src/main/java/jonathan/orellana/apparea/MainActivity.kt
@@ -0,0 +1,36 @@
+package jonathan.orellana.apparea
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val txtBase = findViewById<EditText>(R.id.txtBase)
+        val txtAltura = findViewById<EditText>(R.id.txtAltura)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+
+        val objCalculo  = Calcular()
+
+        btnCalcular.setOnClickListener {
+            val calculo = objCalculo.calculo(txtBase.text.toString().toInt(), txtAltura.text.toString().toInt())
+
+            txtResultado.text = "El área es: " + calculo.toString()
+        }
+    }
+}
